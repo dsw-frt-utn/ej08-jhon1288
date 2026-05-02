f8fc5f2d85195b94abfb6eab2b0d64514d70bf5f
Juan Coronel <jccoronel12.88@gmail.com>
2026-05-01 23:32:16 -0300

    se realizo problema 2

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("===== PROBLEMA 1 =====");
             ProductHelper helper = new ProductHelper();
             Console.WriteLine(helper.ObtenerEtiquetaProducto(123, "Mouse", 1500));
+
+            Console.WriteLine("\n===== PROBLEMA 2 =====");
+            Problema2 p2 = new Problema2();
+            Console.WriteLine(p2.CrearResumenVenta(1001, "Teclado", 2, 1500));
         }
     }
 }
